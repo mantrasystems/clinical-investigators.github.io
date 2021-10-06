9eddb23fa75fe4dc9657d482e633c49e32678994
Mantra Ventures CTO <rico@moscovium.local>
2021-10-06 16:36:55 +0100

    cleaner test

--- docs/assets/js/cleaner.js
+++ docs/assets/js/cleaner.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
   function eraseCookie(name) {   
-    document.cookie = name+'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name+'=; Path=/; Domain=.clinical-investigators.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
 
   eraseCookie('_ga');
