1337d1e6c336871cc05aa68c6d8b8d0bc4c5341d
Mantra Ventures CTO <rico@moscovium.local>
2021-10-06 16:15:51 +0100

    cleaner.js test

--- docs/assets/js/cleaner.js
+++ docs/assets/js/cleaner.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
   function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999; SameSite=Lax';
+    document.cookie = name+'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
 
   eraseCookie('_ga');
